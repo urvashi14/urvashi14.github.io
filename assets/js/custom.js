fb4413c4b68ec8877786c415d9475d3424777cd0
urvashi14 <urvashi.ucoe@gmail.com>
2015-03-07 21:59:57 -0500

    portfolio added

---
+++ assets/js/custom.js
@@ -0,0 +1,71 @@
+﻿
+
+
+$(document).ready(function () {
+
+    // FOR SCROLL ANIMATIONS
+    window.scrollReveal = new scrollReveal();
+
+    // SCROLL SCRIPTS 
+        $('.scroll-me a').bind('click', function (event) { //just pass scroll-me class and start scrolling
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutQuad');
+        event.preventDefault();
+        });
+
+    //COLLAPSE HEADER ON SCRLL
+        $(window).scroll(function () {
+            if ($(".navbar").offset().top > 40) {
+                $(".navbar-fixed-top").addClass("navbar-pad-original");
+            } else {
+                $(".navbar-fixed-top").removeClass("navbar-pad-original");
+            }
+        });
+
+    //FANCY BOX PLUGIN SCRIPT FOR POPUP IMAGE
+        $('.fancybox-media').fancybox({
+            openEffect: 'elastic',
+            closeEffect: 'elastic',
+            helpers: {
+                title: {
+                    type: 'inside'
+                }
+            }
+        });
+
+    //ISOTOPE  PLUGIN SCRIPT FOR FILTER FUCNTIONALITY
+        $(window).load(function () {
+            var $container = $('#portfolio-div');
+            $container.isotope({
+                filter: '*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            $('.caegories a').click(function () {
+                $('.caegories .active').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+
+        });
+
+    //CLIENTS SLIDER
+        $("#clients-slider").carousel({
+            interval: 2000 //TIME IN MILLI SECONDS
+        });
+
+});
